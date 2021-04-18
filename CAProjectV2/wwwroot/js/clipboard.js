78b82dc4562eef04958d90d6b7805034dc5dc5b9
tansizheng89 <e0696890@u.nus.edu>
2021-04-18 11:31:02 +0800

    js for clipboard

---
+++ CAProjectV2/wwwroot/js/clipboard.js
@@ -0,0 +1,18 @@
+﻿window.onload = function () {
+
+    var elemlist = document.getElementsByClassName("actCode");
+
+    for (let j = 0; j < elemlist.length; j++) {
+        if (elemlist[j].options.length === 1)
+            elemlist[j].addEventListener("click", toCopy);
+        else
+            elemlist[j].addEventListener("change", toCopy);
+    }
+}
+function toCopy(event) {
+    let selected = event.currentTarget;
+    let code = selected.options[selected.selectedIndex].innerText;
+    navigator.clipboard.writeText(code)
+    alert("You have copied the activation code :" + code);
+
+}
